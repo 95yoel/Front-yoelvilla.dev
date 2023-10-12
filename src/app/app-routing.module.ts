a8d748dadaf8451668460b5bf65bef16be205c39
Yoel Villa <villayoel@gmail.com>
2023-10-12 18:26:48 +0200

    added rotation to icons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogComponent } from './blog/blog.component';
 import { AboutComponent } from './about/about.component';
+import { AdminComponent } from './users/admin/admin.component';
+import { LoginComponent } from './users/login/login.component';
 
 const routes: Routes = [
   {path: '',component:BlogComponent},
-  {path:'about',component:AboutComponent}
+  {path:'about',component:AboutComponent},
+  {path:'admin',component:AdminComponent},
+  {path:'login',component:LoginComponent}
 ];
 
 @NgModule({
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild,ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  @ViewChild('login') login: ElementRef | undefined;
+  @ViewChild('settings') settings: ElementRef | undefined;
+
+
+  isRotated = false;
+
+  constructor() { }
+
+  // rotateIcon() {
+  //   if (this.login) {
+  //     this.login.nativeElement.style.transition = 'all 1s ease-in-out';
+  //     this.login.nativeElement.style.transform = 'rotate(360deg)';
+  //   }
+  // }
+
+  rotateIcon(element: ElementRef | HTMLElement | undefined) {
+    if (element) {
+      if (element instanceof ElementRef) {
+        element.nativeElement.style.transition = 'all 1s ease-in-out';
+        element.nativeElement.style.transform = 'rotate(360deg)';
+        setTimeout(() => {
+          element.nativeElement.style.transform = 'rotate(0deg)';
+        }, 1000);
+        
+      } else {
+        element.style.transition = 'all 1s ease-in-out';
+        element.style.transform = 'rotate(360deg)';
+        setTimeout(() => {
+          element.style.transform = 'rotate(0deg)';
+        }, 1000);
+
+        
+      }
+    }
+  }
+
+  
+
 
 }
